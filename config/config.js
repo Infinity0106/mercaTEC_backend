57e36e71492456fa3119e14c81080caa180be5a9
Bernardo Orozco Garza <berny.orozco@hotmail.com>
2018-08-23 10:19:32 -0500

    Change config/config.js
    update credentials for DB connection

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
+var Op = require("sequelize").Op;
 module.exports = {
   development: {
     username: "daw_dev",
@@ -6,20 +7,26 @@ module.exports = {
     host: "127.0.0.1",
     dialect: "postgres",
     migrationStorage: "json",
-    seederStorage: "json"
+    seederStorage: "json",
+    operatorsAliases: Op
   },
   test: {
-    username: process.env.CI_DB_USERNAME,
-    password: process.env.CI_DB_PASSWORD,
-    database: process.env.CI_DB_NAME,
+    username: "daw_test",
+    password: "1234567890",
+    database: "DAW_test",
     host: "127.0.0.1",
-    dialect: "mysql"
+    dialect: "postgres",
+    migrationStorage: "json",
+    seederStorage: "json",
+    operatorsAliases: Op,
+    logging: false
   },
   production: {
     username: process.env.PROD_DB_USERNAME,
     password: process.env.PROD_DB_PASSWORD,
     database: process.env.PROD_DB_NAME,
     host: process.env.PROD_DB_HOSTNAME,
-    dialect: "mysql"
+    dialect: "mysql",
+    operatorsAliases: Op
   }
 };
