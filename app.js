e84c0f09d9973585ace7aa1ea247f4913ac2380d
Bernardo Orozco Garza <berny.orozco@hotmail.com>
2018-08-23 10:11:30 -0500

    Add app.js
    changes: instalation of cors, dotenv, express-rate-limit, add api/v1 route for redirection to api

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+// var session = require("client-sessions");
+var RateLimit = require("express-rate-limit");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//load env variables
+require("dotenv").load();
+
+var indexRouterV1 = require("./routes/api/v1/index");
 
 var app = express();
+var limiter = new RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  delayMs: 0 // disable delaying - full speed until the max limit is reached
+});
+
+app.enable("trust proxy");
+
+app.use(cors());
+app.use(limiter);
+// app.use(
+//   session({
+//     cookieName: "session",
+//     secret: "QkhW!pfiLAP5;Ra(?6l/.[dedVM5bDd_Et#&+Z[rEUkXGd*fy/=q-ip[MMw:N+,",
+//     duration: 30 * 60 * 1000,
+//     activeDuration: 5 * 60 * 1000,
+//     httpOnly: true,
+//     secure: true,
+//     ephemeral: true
+//   })
+// );
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+if (process.env.NODE_ENV != "test") {
+  app.use(logger("dev"));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/api/v1", indexRouterV1);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,11 +58,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
