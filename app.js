2836b1b6dd0d5a405105c0354e9a78540f6c025b
Bernardo Orozco Garza <berny.orozco@hotmail.com>
2018-11-13 21:25:08 -0600

    try setup

--- app.js
+++ app.js
@@ -43,10 +43,10 @@ app.use(params.expressMiddleware());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 if (process.env.NODE_ENV != "test") {
